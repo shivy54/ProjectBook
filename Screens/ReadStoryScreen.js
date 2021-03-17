df0cb2c168519531f547731fa8a417f771761f18
shivy54 <59441312+shivy54@users.noreply.github.com>
2021-03-17 18:08:18 +0530

    Add files via upload

---
+++ Screens/ReadStoryScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
+
+
+export default class WriteStoryScreen extends React.Component{
+    render(){
+        return(
+          <View>
+    </View>
+    
+        )
+    }
+}
