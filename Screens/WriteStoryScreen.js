03e1544fae8e135d2b6d8b7e55837e2851ff7a49
shivy54 <59441312+shivy54@users.noreply.github.com>
2021-03-21 16:35:09 +0530

    Add files via upload

--- Screens/WriteStoryScreen.js
+++ Screens/WriteStoryScreen.js
@@ -9,6 +9,9 @@ import {
   TouchableOpacity,
 } from "react-native";
 
+import db from '../config'
+import firebase from 'firebase'
+
 export default class WriteStoryScreen extends React.Component {
   constructor() {
     super();
@@ -18,6 +21,15 @@ export default class WriteStoryScreen extends React.Component {
       Story: "",
     };
   }
+
+  submitStory = (Author, Title, Story) => {
+    db.collection("Story").add({
+      Title: this.state.Title,
+      Author: this.state.Author,
+      Story: this.state.Story,
+    });
+  };
+
   render() {
     return (
       <View>
@@ -52,7 +64,8 @@ export default class WriteStoryScreen extends React.Component {
           }}
           value={this.state.Story}
         />
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity style={styles.button}
+        onPress={this.submitStory}>
           <Text>Request</Text>
         </TouchableOpacity>
       </View>
@@ -60,38 +73,36 @@ export default class WriteStoryScreen extends React.Component {
   }
 }
 const styles = StyleSheet.create({
-  keyBoardStyle : {
-    flex:1,
-    alignItems:'center',
-    justifyContent:'center'
+  keyBoardStyle: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
   },
-  formTextInput:{
-    width:"75%",
-    height:35,
-    alignSelf:'center',
-    borderColor:'#ffab91',
-    borderRadius:10,
-    borderWidth:1,
-    marginTop:20,
-    padding:10,
+  formTextInput: {
+    width: "75%",
+    height: 35,
+    alignSelf: "center",
+    borderColor: "#ffab91",
+    borderRadius: 10,
+    borderWidth: 1,
+    marginTop: 20,
+    padding: 10,
   },
-  button:{
-    width:"75%",
-    height:50,
-    justifyContent:'center',
-    alignItems:'center',
-    borderRadius:10,
-    backgroundColor:"#ff5722",
+  button: {
+    width: "75%",
+    height: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+    backgroundColor: "#ff5722",
     shadowColor: "#000",
     shadowOffset: {
-       width: 0,
-       height: 8,
+      width: 0,
+      height: 8,
     },
     shadowOpacity: 0.44,
     shadowRadius: 10.32,
     elevation: 16,
-    marginTop:20
-    },
-  }
-)
-
+    marginTop: 20,
+  },
+});
