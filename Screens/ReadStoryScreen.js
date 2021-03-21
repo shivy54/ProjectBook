2bfbe300a6116284678296b0636cbe6d66e279ba
shivy54 <59441312+shivy54@users.noreply.github.com>
2021-03-21 15:45:34 +0530

    Add files via upload

--- Screens/ReadStoryScreen.js
+++ Screens/ReadStoryScreen.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
+import ReadStoryScreen from './WriteStoryScreen';
 
 
 export default class WriteStoryScreen extends React.Component{
     render(){
         return(
           <View>
+              <Text>Read Story</Text>
     </View>
     
         )
--- Screens/WriteStoryScreen.js
+++ Screens/WriteStoryScreen.js
@@ -1,29 +1,97 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  Touchable,
+  TouchableOpacity,
+} from "react-native";
 
-
-export default class WriteStoryScreen extends React.Component{
-    render(){
-        return(
-          <View>
-            
-    <TextInput
-      style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-      onChangeText={text => onChangeText(text)}
-      value={value}
-    />
-    <TextInput
-      style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-      onChangeText={text => onChangeText(text)}
-      value={value}
-    />
-    <TextInput
-      style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-      onChangeText={text => onChangeText(text)}
-      value={value}
-    />
-    </View>
-    
-        )
-    }
+export default class WriteStoryScreen extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      Author: "",
+      Title: "",
+      Story: "",
+    };
+  }
+  render() {
+    return (
+      <View>
+        <TextInput
+          style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
+          placeholder={"Title of story"}
+          onChangeText={(text) => {
+            this.setState({
+              Title: text,
+            });
+          }}
+          value={this.state.Title}
+        />
+        <TextInput
+          style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
+          placeholder={"Author Name"}
+          onChangeText={(text) => {
+            this.setState({
+              Author: text,
+            });
+          }}
+          value={this.state.Author}
+        />
+        <TextInput
+          style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
+          multiline={true}
+          placeholder={"Write Story"}
+          onChangeText={(text) => {
+            this.setState({
+              Story: text,
+            });
+          }}
+          value={this.state.Story}
+        />
+        <TouchableOpacity style={styles.button}>
+          <Text>Request</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
+const styles = StyleSheet.create({
+  keyBoardStyle : {
+    flex:1,
+    alignItems:'center',
+    justifyContent:'center'
+  },
+  formTextInput:{
+    width:"75%",
+    height:35,
+    alignSelf:'center',
+    borderColor:'#ffab91',
+    borderRadius:10,
+    borderWidth:1,
+    marginTop:20,
+    padding:10,
+  },
+  button:{
+    width:"75%",
+    height:50,
+    justifyContent:'center',
+    alignItems:'center',
+    borderRadius:10,
+    backgroundColor:"#ff5722",
+    shadowColor: "#000",
+    shadowOffset: {
+       width: 0,
+       height: 8,
+    },
+    shadowOpacity: 0.44,
+    shadowRadius: 10.32,
+    elevation: 16,
+    marginTop:20
+    },
+  }
+)
+
